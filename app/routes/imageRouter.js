387041a96a3ac240322a7541eef2fb68d78b801d
Van Le <70436051+iRok166@users.noreply.github.com>
2022-01-08 23:47:06 +0700

    fix bug upload

--- app/routes/imageRouter.js
+++ app/routes/imageRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
-const { upload_image } = require("../services/multer");
+const {
+  upload_image
+} = require("../services/multer");
 const fs = require("fs");
 const path = require("path");
 
@@ -13,44 +15,52 @@ router.get("/upload-image", (req, res) => {
 
 router.post("/upload-image", upload_image.single("data-image"), async (req, res) => {
   const extAccept = [".png", ".jpeg", ".jpg"];
-  const extname = path.extname(req.file.originalname).toLowerCase();
+  if (req.file) {
+    const extname = path.extname(req.file.originalname).toLowerCase();
 
-  if (extAccept.includes(extname)) {
-    await fs.renameSync(
-      path.join(__dirname, "../public/img/", req.file.originalname),
-      path.join(__dirname, "../public/img/", "image.png"),
-      (err) => {
-        if (err)
-          throw err;
+    if (extAccept.includes(extname)) {
+      await fs.renameSync(
+        path.join(__dirname, "../public/img/", req.file.originalname),
+        path.join(__dirname, "../public/img/", "image.png"),
+        (err) => {
+          if (err)
+            throw err;
+        }
+      );
+      if (await fs.existsSync(path.join(__dirname, "../../image.png"))) {
+        await fs.unlinkSync(path.join(__dirname, "../../image.png"));
       }
-    );
-    if (await fs.existsSync(path.join(__dirname, "../../image.png"))) {
-      await fs.unlinkSync(path.join(__dirname, "../../image.png"));
+      const path_src = path.join(__dirname, "../public/img/image.png");
+      const path_dst = path.join(__dirname, "../../image.png");
+      await fs.copyFile(path_src, path_dst, (err) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log("Copy file done.");
+      });
+      const hasImage = true;
+      return res.render("upload-image", {
+        hasImage,
+      });
+    } else {
+      if (await fs.existsSync(path.join(__dirname, "../../image.png"))) {
+        await fs.unlinkSync(path.join(__dirname, "../../image.png"));
+      }
+      await fs.unlinkSync(path.join(__dirname, '../public/img/', req.file.originalname));
+
+
+      const notSupport = true;
+      return res.render("upload-image", {
+        notSupport,
+      });
     }
-    const path_src = path.join(__dirname, "../public/img/image.png");
-    const path_dst = path.join(__dirname, "../../image.png");
-    await fs.copyFile(path_src, path_dst, (err) => {
-      if (err) {
-        console.log(err);
-      }
-      console.log("Copy file done.");
-    });
-    const hasImage = true;
-    return res.render("upload-image", {
-      hasImage,
-    });
   } else {
-    if (await fs.existsSync(path.join(__dirname, "../../image.png"))) {
-      await fs.unlinkSync(path.join(__dirname, "../../image.png"));
-    }
-    await fs.unlinkSync(path.join(__dirname, '../public/img/', req.file.originalname));
-
-
-    const notSupport = true;
+    const notFile = true
     return res.render("upload-image", {
-      notSupport,
+      notFile,
     });
   }
+
 });
 
 module.exports = router;
--- app/routes/modelRouter.js
+++ app/routes/modelRouter.js
@@ -14,43 +14,51 @@ router.get('/upload-model', (req, res) => {
 });
 
 router.post('/upload-model', upload_model.single('data-model'), async (req, res) => {
-    const extAccept = ['.pt', '.pth']
-    const extname = path.extname(req.file.originalname).toLowerCase()
+    if (req.file) {
+        const extAccept = ['.pt', '.pth']
+        const extname = path.extname(req.file.originalname).toLowerCase()
 
-    if (extAccept.includes(extname)) {
-        await fs.rename(
-            path.join(__dirname, '../public/model/', req.file.originalname),
-            path.join(__dirname, '../public/model/', 'best.pt'),
-            (err) => {
-                if (err) throw err;
+        if (extAccept.includes(extname)) {
+            await fs.rename(
+                path.join(__dirname, '../public/model/', req.file.originalname),
+                path.join(__dirname, '../public/model/', 'best.pt'),
+                (err) => {
+                    if (err) throw err;
+                }
+            );
+            if (await fs.existsSync(path.join(__dirname, "../../best.pt"))) {
+                await fs.unlinkSync(path.join(__dirname, "../../best.pt"));
             }
-        );
-        if (await fs.existsSync(path.join(__dirname, "../../best.pt"))) {
-            await fs.unlinkSync(path.join(__dirname, "../../best.pt"));
+
+            const path_src = path.join(__dirname, "../public/model/best.pt");
+            const path_dst = path.join(__dirname, "../../best.pt");
+            await fs.copyFile(path_src, path_dst, (err) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log("Copy file done.");
+            });
+            const justAdd = true
+            return res.render('upload-model', {
+                justAdd
+            });
+        } else {
+            if (await fs.existsSync(path.join(__dirname, "../../best.pt"))) {
+                await fs.unlinkSync(path.join(__dirname, "../../best.pt"));
+            }
+            await fs.unlinkSync(path.join(__dirname, '../../', req.file.originalname));
+            const notSupport = true
+            return res.render('upload-model', {
+                notSupport
+            });
         }
-
-        const path_src = path.join(__dirname, "../public/model/best.pt");
-        const path_dst = path.join(__dirname, "../../best.pt");
-        await fs.copyFile(path_src, path_dst, (err) => {
-            if (err) {
-                console.log(err);
-            }
-            console.log("Copy file done.");
-        });
-        const justAdd = true
-        return res.render('upload-model', {
-            justAdd
-        });
     } else {
-        if (await fs.existsSync(path.join(__dirname, "../../best.pt"))) {
-            await fs.unlinkSync(path.join(__dirname, "../../best.pt"));
-        }
-        await fs.unlinkSync(path.join(__dirname, '../../', req.file.originalname));
-        const notSupport = true
-        return res.render('upload-model', {
-            notSupport
+        const notFile = true
+        return res.render("upload-model", {
+            notFile,
         });
     }
+
 
 });
 
